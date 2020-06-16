c99c76cf1d087b8ac0deb8b6d4b84be0c4b05ad4
Rishab Jain <rjrishabh.jain5@gmail.com>
2020-06-16 08:01:48 +0530

    Updates

---
+++ precache-manifest.bfd6d5ee9073452f5bd22993ec8697d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9faa1daeee880a1f4f7aa7bf3eef6f86",
+    "url": "/Rishab-j/learning-React/index.html"
+  },
+  {
+    "revision": "1a2c8e14bd1b4f374146",
+    "url": "/Rishab-j/learning-React/static/css/main.14c9000e.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/Rishab-j/learning-React/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rishab-j/learning-React/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2c8e14bd1b4f374146",
+    "url": "/Rishab-j/learning-React/static/js/main.e3d1c702.chunk.js"
+  },
+  {
+    "revision": "5404fcb934f329d4f372",
+    "url": "/Rishab-j/learning-React/static/js/runtime-main.4cbb5002.js"
+  }
+]);
